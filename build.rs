8f45b8b2f5c77091247a1c99cf9e77aceb846254
Michael Malyshev <michaelm@altia.com>
2020-01-28 11:46:35 +0300

    Fix build on windows platforms
    This fix addresses https://github.com/houqp/leptess/issues/3
    
    This library on Windows is distributed using VCPKG
    (https://github.com/microsoft/vcpkg). This commit uses crate 'vcpgk' to
    locate installed libraries and automatically provide library name for
    cargo.
    
    bindgen needs extra include directory to locate library headers. It
    is implemented using clang_arg() method.
    
    VCPKG is also available on Linux and MacOS but the most of users use
    standard package manages so vcpkg is called on Windows only

--- build.rs
+++ build.rs
@@ -2,12 +2,37 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
+#[cfg(windows)]
+use vcpkg;
+
+#[cfg(windows)]
+fn find_leptonica_system_lib() -> Option<String> {
+    let lib = vcpkg::Config::new().find_package("leptonica").unwrap();
+
+    let include = lib
+        .include_paths
+        .iter()
+        .map(|x| x.to_string_lossy())
+        .collect::<String>();
+    Some(include)
+}
+
+#[cfg(not(windows))]
+fn find_leptonica_system_lib() -> Option<String> {
+    println!("cargo:rustc-link-lib=lept");
+    None
+}
 
 fn main() {
-    println!("cargo:rustc-link-lib=lept");
+    let clang_extra_include = find_leptonica_system_lib();
 
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+    let mut bindings = bindgen::Builder::default().header("wrapper.h");
+
+    if let Some(include_path) = clang_extra_include {
+        bindings = bindings.clang_arg(format!("-I{}", include_path));
+    }
+
+    let bindings = bindings
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
