20e70d396381835d632274dedfd0da390be73d0f
Chris Couzens <chris.couzens@loveholidays.com>
2021-12-09 22:08:39 +0000

    fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,8 @@ mod tests {
     #[test]
     fn image_size() {
         unsafe {
-            let image = pixRead(CStr::from_bytes_with_nul_unchecked(b"../test image.png\0").as_ptr());
+            let image =
+                pixRead(CStr::from_bytes_with_nul_unchecked(b"../test image.png\0").as_ptr());
             assert_eq!((*image).w, 1000);
             assert_eq!((*image).h, 500);
             pixFreeData(image);
