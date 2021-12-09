f9b72c40eb7e5f5a465b2a40a34eb5095558978c
Chris Couzens <chris.couzens@loveholidays.com>
2021-12-09 22:11:39 +0000

    Fix warning about cstring not living long enoug (#10)
    * Fix warning about cstring not living long enough

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,13 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 #[cfg(test)]
 mod tests {
     use super::{pixFreeData, pixRead};
-    use std::ffi::CString;
+    use std::ffi::CStr;
 
     #[test]
     fn image_size() {
         unsafe {
-            let image = pixRead(CString::new("../test image.png").unwrap().as_ptr());
+            let image =
+                pixRead(CStr::from_bytes_with_nul_unchecked(b"../test image.png\0").as_ptr());
             assert_eq!((*image).w, 1000);
             assert_eq!((*image).h, 500);
             pixFreeData(image);
