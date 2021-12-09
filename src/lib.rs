38974a169ea7dce9401031fe6d30577752048629
Chris Couzens <chris.couzens@loveholidays.com>
2021-12-09 12:32:41 +0000

    Fix warning about cstring not living long enoug

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ mod tests {
     #[test]
     fn image_size() {
         unsafe {
-            let image = pixRead(CString::new("../test image.png").unwrap().as_ptr());
+            let image = pixRead(CStr::from_bytes_with_nul_unchecked(b"../test image.png\0").as_ptr());
             assert_eq!((*image).w, 1000);
             assert_eq!((*image).h, 500);
             pixFreeData(image);
