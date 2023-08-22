fa2f82358bf4bd9d59bcef97b81a81df4472a0e0
Chris Couzens <ccouzens@gmail.com>
2023-08-22 08:00:30 +0100

    Do not warn on u128 types in the C FFI
    https://github.com/ccouzens/leptonica-sys/issues/3
    
    I do not see this on my work ARM MacOS laptop, but on do see it on
    my personal x86-64 Linux laptop. Therefore, I'm restricting the
    skip to just that configuration.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,14 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+// Do not warn on u128 types in the C FFI.
+// This can be observed on x86-64 Linux, such as on the GitHub Actions.
+// https://github.com/ccouzens/leptonica-sys/issues/3
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+#[allow(improper_ctypes)]
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(any(not(target_os = "linux"), not(target_arch = "x86_64")))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
